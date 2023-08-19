6242a7a608dc8ff90cc31881155d899cc83bbc38
oupsfed <oups.fedorov@gmail.com>
2023-08-19 15:02:54 +0300

    добавил postgreSQL для actions

--- artele/artele/settings.py
+++ artele/artele/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'artele.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'NAME': os.getenv('POSTGRES_DB', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='3654'),
         'HOST': os.getenv('DB_HOST', default='db'),
